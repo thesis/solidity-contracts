ab9a026b4bca0b1753f046a6b8a3dc97d7ec6031
Piotr Dyraga <piotr.dyraga@keep.network>
2021-07-27 12:48:47 +0200

    Unit tests for CloneFactory contract

---
+++ test/helpers/contract-test-helpers.js
@@ -0,0 +1,20 @@
+// FIXME Retrieves past events. This is a workaround for a known issue described
+// FIXME here: https://github.com/nomiclabs/hardhat/pull/1163
+// FIXME The preferred way of getting events would be using listners:
+// FIXME https://docs.ethers.io/v5/api/contract/contract/#Contract--events
+function pastEvents(receipt, contract, eventName) {
+  const events = []
+
+  for (const log of receipt.logs) {
+    if (log.address === contract.address) {
+      const parsedLog = contract.interface.parseLog(log)
+      if (parsedLog.name === eventName) {
+        events.push(parsedLog)
+      }
+    }
+  }
+
+  return events
+}
+
+module.exports.pastEvents = pastEvents
