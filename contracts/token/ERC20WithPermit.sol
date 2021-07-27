8c4693de2747e4cad52ed24c9b4d75fd970e7232
Piotr Dyraga <piotr.dyraga@keep.network>
2021-07-27 10:43:50 +0200

    Explained logic behind checking current chain ID against the current one

--- contracts/token/ERC20WithPermit.sol
+++ contracts/token/ERC20WithPermit.sol
@@ -161,6 +161,13 @@ contract ERC20WithPermit is IERC20WithPermit, Ownable {
 
     /* solhint-disable-next-line func-name-mixedcase */
     function DOMAIN_SEPARATOR() public view override returns (bytes32) {
+        // As explained in EIP-2612, if the DOMAIN_SEPARATOR contains the
+        // chainId and is defined at contract deployment instead of
+        // reconstructed for every signature, there is a risk of possible replay
+        // attacks between chains in the event of a fututre chain split.
+        // To address this issue, we check the cached chain ID against the
+        // current one and in case they are different, we build domain separator
+        // from scratch.
         if (chainId() == cachedChainId) {
             return cachedDomainSeparator;
         } else {
